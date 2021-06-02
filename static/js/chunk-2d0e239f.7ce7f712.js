f9d066c7d28dc368b300ae859e8ddffd175215a7
zww <cqzww@sina.com>
2021-06-02 09:51:35 +0800

    test

---
+++ static/js/chunk-2d0e239f.7ce7f712.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e239f"],{"7e94":function(e,n,c){"use strict";c.r(n);var l=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div",[c("a-card",[c("label",[e._v("详情")])])],1)},t=[],a=c("0c7c"),u={},r=Object(a["a"])(u,l,t,!1,null,null,null);n["default"]=r.exports}}]);
